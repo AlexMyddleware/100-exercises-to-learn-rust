44a28975fe834226169f2beb68eff86fb8fe28bb
AlexMyddleware <alexistroitzky@myddleware.com>
2024-08-11 15:06:37 +0200

    not yet, should put single - instead of = for the function return type

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -11,7 +11,12 @@
 // Use only what you learned! No loops yet, so you'll have to use recursion!
 
 fn factorial(n:u32) => u32 {
-    n - factorial(n - 1)
+    if n <= 1 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
+
 }
 
 #[cfg(test)]
