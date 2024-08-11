15f16ba0f459c3c2e83106f8da8c00c4a3c235c6
AlexMyddleware <alexistroitzky@myddleware.com>
2024-08-11 18:37:45 +0200

    Update lib.rs

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -1,20 +1,16 @@
 // Rewrite the factorial function using a `while` loop.
 pub fn factorial(n: u32) -> u32 {
     // We pretend that n is going to be 3, and the result should be 6
-    let mut result: u32 = 1; // here we initialize the result which will be returned at the end of the function, at the beginning it is always 1 and this is why factorial of 1 will be 1
+   let mut bud:u32 = 1;
 
-    let mut i = n; // here we initialize i, the counter, and at the start it is always equal to n, so here it will be 3
+   let mut malenia:u32 = n;
 
-    // now we are going to do a while loop
+   while malenia > 1 {
+    bud *= malenia;
+    malenia -= 1;
+   }
 
-    //we compare i to 1, and we use the > sign. That means that i starts at 3, then it goes to 2, and when it reaches 1 we don't enter the loop
-
-    while i > 1 { // no parenthesis just like the if statement
-        result *= i; //here we multiply result by i, so in the first step it is going to be 1 * 3 which is 3
-        i -= 1; // here we reduce i by one, so at the beginning i was 3, and now it is 2
-    }
-
-    result
+   bud
 }
 
 #[cfg(test)]
