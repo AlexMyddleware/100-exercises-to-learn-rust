4606cc3da1b330478e5d4f644cd8d5c49a4b3d64
AlexMyddleware <alexistroitzky@myddleware.com>
2024-08-12 18:18:35 +0200

    Update lib.rs

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,12 +1,14 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    let mut result:u32 = 1;
+let mut result:u32 = 1;
+
     for i in (2..=n) {
         result *= i;
     }
 
     result
-}
+
+    }
 
 #[cfg(test)]
 mod tests {
