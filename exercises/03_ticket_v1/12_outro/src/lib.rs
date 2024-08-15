6d8546b5e36e1374584c6d530b422c126d5ee1c3
AlexMyddleware <alexistroitzky@myddleware.com>
2024-08-15 08:36:23 +0200

    forgot to put the checkers in the section, might remember that next time

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,83 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order { // the struct is public
+    product_name: String,
+    quantity: u32,
+    unit_price: u32,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+
+        Order::check_product_name(&product_name);
+        Order::product_name_300(&product_name);
+        Order::check_quantity(quantity);
+        Order::check_unit_price(unit_price);
+        
+
+        Self {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, product_name: String) {
+        Order::check_product_name(&product_name);
+        Order::product_name_300(&product_name);
+        self.product_name = product_name;
+    }
+
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+
+    pub fn set_quantity(&mut self, quantity: u32) {
+        Order::check_quantity(quantity);
+        self.quantity = quantity;
+    }
+
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        Order::check_unit_price(unit_price);
+        self.unit_price = unit_price;
+    }
+
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+
+    fn check_product_name(product_name: &str) {
+        if product_name.is_empty() {
+            panic!("Product name cannot be empty");
+        }
+    }
+
+    // function for product name that can't be longer than 300 bytes
+    fn product_name_300(product_name: &str) {
+        if product_name.len() > 300 {
+            panic!("Product name cannot be longer than 300 bytes");
+        }
+    }
+
+    fn check_quantity(quantity: u32) {
+        if quantity <= 0 {
+            panic!("Quantity must be greater than zero");
+        }
+    }
+
+    fn check_unit_price(unit_price: u32) {
+        if unit_price <= 0 {
+            panic!("Unit price must be greater than zero");
+        }
+    }
+}
