522328357c8002a62b98a7ceebd2beccfcba00db
AlexMyddleware <alexistroitzky@myddleware.com>
2024-08-31 08:50:39 +0200

    we use trim to remove trailing whitespaces

--- exercises/04_traits/07_deref/src/lib.rs
+++ exercises/04_traits/07_deref/src/lib.rs
@@ -1,8 +1,8 @@
 // TODO: whenever `title` and `description` are returned via their accessor methods, they
-//   should be normalized—i.e. leading and trailing whitespace should be removed.
-//   There is a method in Rust's standard library that can help with this, but you won't
-//   find it in the documentation for `String`.
-//   Can you figure out where it is defined and how to use it?
+// TODO   should be normalized—i.e. leading and trailing whitespace should be removed.
+// TODO   There is a method in Rust's standard library that can help with this, but you won't
+// TODO   find it in the documentation for `String`.
+// TODO   Can you figure out where it is defined and how to use it?
 
 pub struct Ticket {
     title: String,
@@ -12,11 +12,11 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn title(&self) -> &str {
-        todo!()
+        &self.title.trim()
     }
 
     pub fn description(&self) -> &str {
-        todo!()
+        &self.description.trim()
     }
 }
 
